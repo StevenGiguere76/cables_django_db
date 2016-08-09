53ad029b073f215ad5975df8d40bb73106d788f3
root <root@steveng-pc>
2016-08-09 11:24:52 -0700

    Adding this

--- cables_db/modules/management/commands/import_test_results.py
+++ cables_db/modules/management/commands/import_test_results.py
@@ -17,19 +17,28 @@ from datetime import date
 import logging
 log = logging.getLogger('cables_db')
 
+
 class Command(BaseCommand):
+
     def add_arguments(self, parser):
         parser.add_argument('device_name', help='The current device being used for testing.')
         parser.add_argument('--start_port_num', help='Test modules start at this port number.')
         parser.add_argument('--device_ip', help='The IP address for a device if it can not use hostname.')
+        parser.add_argument('--speed', help='The speed that was used for testing.')
         parser.add_argument('--end_port_num', help='Test modules end at this port number.')
+        parser.add_argument('--no_qsa_used', action='store_true', help='QSA used at any point during this test run.')
+        parser.add_argument('--breakout_ports', action='store_true', help='Breakout ports are used in this test run.')
 
     def _find_cable_module(self, ethtool_dict):
         try:
-            found_module = CableModule.objects.get(pin_number__exact=ethtool_dict['vendor_pn'], serial_number__exact=ethtool_dict['vendor_sn'])
-        except CableModules.DoesNotExist as e:
-            pass
-        except CableModules.MultipleObjectsReturned as e:
+            found_module = CableModule.objects.get(
+                pin_number__exact=ethtool_dict['vendor_pn'],
+                serial_number__exact=ethtool_dict['vendor_sn'])
+        except CableModule.DoesNotExist as e:
+            raise e
+        except CableModule.MultipleObjectsReturned as e:
+            import ipdb
+            ipdb.set_trace()
             # If multiple objects are returned, the query went very poorly, for now pass error up
             raise e
 
@@ -49,7 +58,7 @@ class Command(BaseCommand):
 
             # Replace the spaces in key with _ and lower
             key = key.replace(' ', '_').lower()
-            
+
             eth_dict[key] = value
         return eth_dict
 
@@ -68,42 +77,46 @@ class Command(BaseCommand):
 
         speed_used = speed_used.split(':')[1]
         speed_used = speed_used.strip()
-       
+
         # Remove the Mb/s piece
         speed_used = speed_used.replace('Mb/s', '')
 
         # Convert to int and divide by 1000
-        return (int(speed_used)/1000)
+        return (int(speed_used) / 1000)
 
-    def _already_tested(self, platform, cable_module):
+    def _already_tested(self, platform, cable_module, speed):
         # Check if this module, for this platform, was already tested TODAY
         try:
-            temp_test_result = TestResult.objects.get(platform=platform, module=cable_module, date_tested=date.today())
+            temp_test_result = TestResult.objects.get(
+                platform=platform, module=cable_module, date_tested=date.today(), speed=speed)
         except TestResult.DoesNotExist:
             return False
-        
+
         return True
 
     def handle(self, *args, **options):
         # Load the device in question SSH and platform from platform DB
-        platform_model = options['device_name'].rsplit('-', 1)[0] # This removes the '-0X' number from end
+        platform_model = options['device_name'].rsplit('-', 1)[0]  # This removes the '-0X' number from end
 
         try:
             platform = Platform.objects.get(platform_name__exact=platform_model)
         except Platform.DoesNotExist:
             # Exit out early
             pass
-    
+
         # Create SSH client, use device IP if for some reason hostname is known not working (moon rack device)
         if options['device_ip']:
             self._start_ssh_connection(options['device_ip'])
         else:
             self._start_ssh_connection(options['device_name'])
 
-        # For each port, load a test result 
-        for port_num in range(int(options['start_port_num']), int(options['end_port_num'])):
+        # For each port, load a test result
+        for port_num in range(int(options['start_port_num']), int(options['end_port_num']) + 1):
             # Call 'ethtool -m' to find what is being used in the port, only need vendor info
-            ethtool_out = self._device_sudo_command('ethtool -m swp{0} | grep -i vendor'.format(port_num))
+            if not options['breakout_ports']:
+                ethtool_out = self._device_sudo_command('ethtool -m swp{0} | grep -i vendor'.format(port_num))
+            else:
+                ethtool_out = self._device_sudo_command('ethtool -m swp{0}s0 | grep -i vendor'.format(port_num))
 
             # Load all the data into a dictonary
             ethtool_data_dict = self._load_ethtool_data(ethtool_out)
@@ -111,40 +124,56 @@ class Command(BaseCommand):
             # Find the module in the current cable DB, if it can't be found skip
             try:
                 cable_module_tested = self._find_cable_module(ethtool_data_dict)
-            except CableModules.MultipleObjectsReturned:
+            except CableModule.MultipleObjectsReturned:
                 # Can't load this module, log it and move on
-                log.info("Could not load module in swp{0}".format(port_num))   
+                log.info("Could not load module in swp{0}, too many entries found.".format(port_num))
+                continue
+            except CableModule.DoesNotExist:
+                log.info("Could not load module in swp{0}, no entries found.".format(port_num))
                 continue
 
+            # Get the current speed of the module
+            # speed_tested = self._get_speed_used(port_num)
+            speed_tested = int(options['speed'])
+
             # Is there already a test result for this module
-            if self._already_tested(platform, cable_module_tested):
+            if self._already_tested(platform, cable_module_tested, speed_tested):
                 log.info('{0} was already tested, skipping.'.format(cable_module_tested))
                 continue
 
-            # Get the current speed of the module 
-            speed_tested = self._get_speed_used(port_num)
-
             # Ask user is QSA was used
-            qsa_used = raw_input('{0} QSA Used? '.format(cable_module_tested))
-
-            if qsa_used.lower() == 'yes':
-                qsa_used_bool = True
-            else:
+            if options['no_qsa_used']:
                 qsa_used_bool = False
+            else:
+                qsa_used = raw_input('{0} QSA Used? '.format(cable_module_tested))
+
+                if qsa_used.lower() == 'yes'or qsa_used.lower() =='y':
+                    qsa_used_bool = True
+                else:
+                    qsa_used_bool = False
 
             # Ask user if module pass/fail
             pass_or_fail = raw_input('{0} Pass or Fail? '.format(cable_module_tested))
-            pass_or_fail = pass_or_fail.upper() # Always uppercase  
+            pass_or_fail = pass_or_fail.upper()  # Always uppercase
+
             if pass_or_fail == 'FAIL':
+                pass_or_fail_bool = False
                 bug_id_url = raw_input('Please enter the URL of the bug-id for this module: ')
             else:
+                pass_or_fail_bool = True
                 bug_id_url = None
 
             # Create test result
-            new_test_result = TestResult(platform=platform, module=cable_module_tested, test_passed=pass_or_fail, speed=speed_tested, qsa_used=qsa_used_bool, bug_id=bug_id_url)
+            new_test_result = TestResult(
+                platform=platform,
+                module=cable_module_tested,
+                test_passed=pass_or_fail_bool,
+                speed=speed_tested,
+                qsa_used=qsa_used_bool,
+                bug_id=bug_id_url)
 
             print(new_test_result)
-            
+
             is_good = raw_input('Does above result look good? ')
             if is_good.lower() == 'yes' or is_good.lower() == 'y':
                 new_test_result.save()
